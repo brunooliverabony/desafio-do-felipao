1f97da481cf75fa68cafe0b17ec5ac19ac47751c
brunooliveirabony <brunooliveirabony@gmail.com>
2024-09-02 07:33:51 -0300

    entregar projeto

--- src/index.js
+++ src/index.js
@@ -1,2 +1,21 @@
-let nome = "Bruno"
-console.log (nome)
+
+let xp = 7000
+
+
+if (xp <= 1000){
+console.log(usuario + "Ferro")}
+if (xp >= 1001 && xp <= 2000){
+console.log(usuario +"Bronze")}
+if ((xp >= 2001) && (xp <= 5000)){
+console.log(usuario +"Prata")}
+if ((xp >= 5001) && (xp <= 7000)){
+console.log(usuario +"Ouro")}
+if ((xp >= 7001) && (xp <= 8000)){
+console.log(usuario +"platina")}
+if ((xp >= 8001) && (xp <= 9000)){
+console.log(usuario +"Ascendente")}
+if ((xp >= 9001) && (xp <= 10000)){
+console.log(usuario +"imortal")}
+if ((xp >= 10001)){
+console.log(usuario +"radiante")}
+
